c6ebcc3804a30893276feac4d477b14ee161ab9a
Zuzana Kuřátková <zkuratkova@gmail.com>
2021-04-11 13:16:17 +0200

    eventListener pro tlačítka

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 console.log('Funguju');
 
 let player = 'circle';
-let playerElm = document.querySelector('#player');
+const playerElm = document.querySelector('#player');
+const btnElm = document.querySelectorAll('.field__cell');
 
 const switchPlayer = () => {
   if (player === 'circle') {
@@ -13,3 +14,19 @@ const switchPlayer = () => {
     playerElm.src = 'images/circle.svg';
   }
 };
+
+btnElm.forEach((cell) => {
+  cell.addEventListener('click', (event) => {
+    if (
+      !event.target.classList.contains('field__cell--circle') &&
+      !event.target.classList.contains('field__cell--cross')
+    ) {
+      if (player === 'circle') {
+        event.target.classList.add('field__cell--circle');
+      } else {
+        event.target.classList.add('field__cell--cross');
+      }
+      switchPlayer();
+    }
+  });
+});
